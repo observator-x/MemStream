a21bda3d62c91e6df124bd85d88f7575d47f489e
observator-x <57844207+observator-x@users.noreply.github.com>
2020-08-03 00:19:21 +0200

    Added support for ESP32

--- MemStream.h
+++ MemStream.h
@@ -25,7 +25,11 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 #include <inttypes.h>
 #include <Stream.h>
-#include <avr/pgmspace.h>
+#ifndef __AVR__
+   #include <pgmspace.h>
+#else
+   #include <avr/pgmspace.h>
+#endif 
 
 class MemStream : public Stream
 {
